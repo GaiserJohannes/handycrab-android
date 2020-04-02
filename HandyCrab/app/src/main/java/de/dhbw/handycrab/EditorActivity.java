64f739247c9350ecc0b1c4878ba7eaa98e80c9c1
GrafZeppelin <jojo98.hannes@gmail.com>
2020-04-02 19:38:47 +0200

    init activities

---
+++ HandyCrab/app/src/main/java/de/dhbw/handycrab/EditorActivity.java
@@ -0,0 +1,13 @@
+package de.dhbw.handycrab;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+}
---
+++ HandyCrab/app/src/main/java/de/dhbw/handycrab/SearchActivity.java
@@ -0,0 +1,25 @@
+package de.dhbw.handycrab;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+
+    /** Called when the user taps the Send button */
+    public void switchRadius(View view) {
+        findViewById(R.id.radius1).setBackgroundTintList(ColorStateList.valueOf(Color.TRANSPARENT));
+        findViewById(R.id.radius2).setBackgroundTintList(ColorStateList.valueOf(Color.TRANSPARENT));
+        findViewById(R.id.radius3).setBackgroundTintList(ColorStateList.valueOf(Color.TRANSPARENT));
+        view.setBackgroundTintList(ColorStateList.valueOf(0xFFFF503C));
+    }
+
+}
