01ba677f47874cf682073115ae5ea7ff8830ac03
gamlicuss <joelschmidwitt@gmail.com>
2020-04-24 09:46:56 +0200

    added errormessages, modified errorhandling

--- HandyCrab/app/src/main/java/de/dhbw/handycrab/backend/BackendConnectionException.java
+++ HandyCrab/app/src/main/java/de/dhbw/handycrab/backend/BackendConnectionException.java
@@ -1,5 +1,9 @@
 package de.dhbw.handycrab.backend;
 
+import android.content.Context;
+import android.content.res.Resources;
+
+import de.dhbw.handycrab.R;
 import de.dhbw.handycrab.model.ErrorCode;
 
 public class BackendConnectionException extends RuntimeException {
@@ -20,6 +24,26 @@ public class BackendConnectionException extends RuntimeException {
 
     public String getMessage(){
         return  getErrorCode().toString();
+    }
+
+    public String getDetailedMessage(Context context){
+        switch (errorCode){
+            case NO_CONNECTION_TO_SERVER: return context.getString(R.string.noConnectionToServerError);
+            case INCOMPLIETE: return context.getString(R.string.incompleteError);
+            case UNAUTHORIZED: return context.getString(R.string.unauthorized);
+            case EMAIL_ALREADY_ASSIGNED: return context.getString(R.string.emailAlreadyAssignedError);
+            case USERNAME_ALREADY_ASSIGNED: return context.getString(R.string.usernameAlreadyAssignedError);
+            case INVALID_EMAIL: return context.getString(R.string.invalidEmailError);
+            case INVALID_LOGIN: return context.getString(R.string.invalidLoginError);
+            case USER_NOT_FOUND: return context.getString(R.string.userNotFound);
+            case INVALID_GEO_LOCATION: return context.getString(R.string.invalidGeoLocation);
+            case BARRIER_NOT_FOUND: return context.getString(R.string.barrierNotFound);
+            case INVALID_USER_ID: return context.getString(R.string.invalidUserId);
+            case SOLUTION_NOT_FOUND: return context.getString(R.string.solutionNotFound);
+            case INVALID_USERNAME: return context.getString(R.string.invalidUsernameError);
+            case INVALID_PASSWORD: return context.getString(R.string.invalidPasswordError);
+            default: return context.getString(R.string.unknownError);
+        }
     }
 
     public int getHttpStatusCode() {
