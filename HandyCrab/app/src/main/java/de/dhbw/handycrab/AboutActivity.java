4471370affb25d807a25f1cfba060d61ba43fd3c
GrafZeppelin <jojo98.hannes@gmail.com>
2020-05-08 20:28:41 +0200

    finish also for about activity

--- HandyCrab/app/src/main/java/de/dhbw/handycrab/AboutActivity.java
+++ HandyCrab/app/src/main/java/de/dhbw/handycrab/AboutActivity.java
@@ -1,6 +1,7 @@
 package de.dhbw.handycrab;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity {
@@ -9,5 +10,16 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
